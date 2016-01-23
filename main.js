d0bcc31a1288601c1db80c0806029ee52fc27279
kingdido999 <kingdido999@gmail.com>
2016-01-22 20:43:24 -0500

    handle not enough songs

--- main.js
+++ main.js
@@ -4,95 +4,175 @@ const electron = require('electron');
 const Moefou = require('moefou');
 const Player = require('player');
 const notifier = require('node-notifier');
+const path = require('path');
 
 const app = electron.app;
-const Menu = electron.Menu;
 const Tray = electron.Tray;
+const Menu = electron.Menu;
+const MenuItem = electron.MenuItem;
 
 const moefou = new Moefou('a8d18630d266f5ad6979c22e18d31ff4056a24105');
 const player = new Player([]);
 
 var appIcon = null;
+var menuInit = null;
+var menuPlaying = null;
+var menuPaused = null;
 
-// template for menu items
-var template = [
+/**
+ * A state when the player is switching to next song before the song
+ * plays out, it prevents multiple songs playing at the same time
+ * caused by clicking 'Next' too frequently.
+ * @type {Boolean}
+ */
+var isSwitchingSong = false;
+
+app.on('ready', function(){
+  appIcon = new Tray('icon.png');
+  menuInit = Menu.buildFromTemplate(templateInit);
+  menuPlaying = Menu.buildFromTemplate(templatePlaying);
+  menuPaused = Menu.buildFromTemplate(templatePaused);
+  appIcon.setContextMenu(menuInit);
+});
+
+var templateInit = [
   {
     label: 'Play',
-    click: function(item) {
-      // get playlist
-      moefou.listenPlaylist({}, function(error, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          var playlist = body.response.playlist;
-
-          // add songs
-          for (var i = 0; i < playlist.length; i++) {
-            console.log('Adding: ' + playlist[i].title);
-            var song = playlist[i];
-
-            player.add({
-              'src': song.url,
-              'title': song.title,
-              'artist': song.artist,
-              'cover': {
-                'small': song.cover.small
-              }
-            })
-          }
-
-          player.play();
-        }
+    click: function() {
+      appIcon.setContextMenu(menuPlaying);
+      fetchSongs(function() {
+        player.play();
       });
     }
   },
   {
+    label: 'Quit',
+    click: function() {
+      app.quit();
+    }
+  }
+];
+
+var templatePlaying = [
+  {
     label: 'Pause',
     click: function() {
+      appIcon.setContextMenu(menuPaused);
       player.pause();
     }
   },
   {
     label: 'Next',
     click: function() {
-      player.next();
+      appIcon.setContextMenu(menuPlaying);
+      nextSong();
     }
   },
   {
     label: 'Quit',
     click: function() {
-      player.stop();
       app.quit();
     }
   }
 ];
 
+var templatePaused = [
+  {
+    label: 'Resume',
+    click: function() {
+      appIcon.setContextMenu(menuPlaying);
+      player.pause();
+    }
+  },
+  {
+    label: 'Next',
+    click: function() {
+      appIcon.setContextMenu(menuPlaying);
+      nextSong();
+    }
+  },
+  {
+    label: 'Quit',
+    click: function() {
+      app.quit();
+    }
+  }
+];
 
-app.on('ready', function(){
-  appIcon = new Tray('icon.png');
-  var contextMenu = Menu.buildFromTemplate(template);
-  appIcon.setToolTip('萌否电台');
-  appIcon.setContextMenu(contextMenu);
-});
-
-// event: on playing
 player.on('playing',function(song){
-  // console.log('Playing: ' + JSON.stringify(song, null, 4));
+  console.log('Now playing: ' + song.title);
+  isSwitchingSong = false;
 
+  // show notification panel
   notifier.notify({
     'title': 'Now playing: ',
-    'message': song.title + ' / ' + song.artist
+    'message': song.title + (song.artist ? ' | ' + song.artist : ''),
+    'icon': path.join(__dirname, 'notify-icon.png')
   });
 });
 
-// event: on playend
-player.on('playend',function(item){
-  // return a playend item
-  console.log('src:' + item + ' play done, switching to next one ...');
-});
-
-// event: on error
 player.on('error', function(err){
-  // when error occurs
   console.log(err);
 });
+
+/**
+ * Fetch songs and add them to the music list.
+ * @param  {Function} callback
+ */
+function fetchSongs(callback) {
+  console.log('Fetching songs...');
+  moefou.listenPlaylist({}, function(error, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      let playlist = body.response.playlist;
+
+      // add songs
+      for (var i = 0; i < playlist.length; i++) {
+        console.log('Adding: ' + playlist[i].title);
+        let song = playlist[i];
+
+        player.add({
+          'src': song.url,
+          'title': song.title,
+          'artist': song.artist
+        });
+      }
+
+      callback();
+    }
+  });
+}
+
+/**
+ * Go to next song, fetch songs if necessary.
+ */
+function nextSong() {
+  if (isSwitchingSong) {
+    return;
+  } else {
+    console.log('Next song...');
+    isSwitchingSong = true;
+
+    if (notEnoughSongs()) {
+      fetchSongs(function() {
+        player.next();
+      });
+    } else {
+      player.next();
+    }
+  }
+}
+
+/**
+ * Check if the list has enough songs.
+ * @return {boolean}
+ */
+function notEnoughSongs() {
+  let current = player.playing;
+  let nextIndex = player.options.shuffle ?
+    chooseRandom(_.difference(list, [current._id])) :
+    current._id + 1;
+
+  return nextIndex >= player.list.length;
+}
